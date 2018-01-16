f6c14b790c59e783735cfc32ef6776565e91ad61
dasa5 <dasa5@LAPTOP-M10ARQPP>
2018-01-16 23:15:52 +0530

    kh

---
+++ test/src/test/aaaaa.java
@@ -0,0 +1,14 @@
+package test;
+
+public class aaaaa {
+
+	public aaaaa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
